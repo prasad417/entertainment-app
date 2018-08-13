96461b6f97dca04a69a9d636caae77cf47eaa0c0
prasad <417.prasad@gmail.com>
2018-08-13 11:08:17 -0500

    Adding Home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 import { HeaderModule } from './navigation/header/header.module';
 import { FooterModule } from './navigation/footer/footer.module';
@@ -11,21 +13,28 @@ import { LanguagesModule } from './languages/languages-module';
 import { ArtistsModule } from './artists/artists.module';
 import { MoviesModule } from './movies/movies-module';
 
+const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch: 'full'}
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     // Angular modules
     BrowserModule,
     CommonModule,
     FormsModule,
+    RouterModule.forRoot(routes),
     // Custom modules
     HeaderModule,
     FooterModule,
     LanguagesModule,
     ArtistsModule,
-    MoviesModule
+    MoviesModule,
+    // Routing
   ],
   providers: [],
   bootstrap: [AppComponent]
